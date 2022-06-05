d09da20ad2afc231371774c0dd026a63c7132937
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-05 09:21:01 +0300

    Добавлен интерфейс репозитория товара

---
+++ internet-shop_ct/RepositoryInterfaces/IProductsRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace internet_shop_ct.RepositoryInterfaces
+{
+    public interface IProductsRepository<T>
+    {
+        T[] GetAll(); //Получить все товары
+
+        T GetByProductCode(int product_code); //Получить товар по его коду
+
+        T Add(T newProduct); //Добавить товар
+
+        T Update(T existingProduct); //Обновить данные товара
+
+        void Delete(T existingProduct); //Удалить товар
+
+    }
+}
