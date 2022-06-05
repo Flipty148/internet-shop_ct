601f0044b8a8095407f11654c9639b5e0cb261c4
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-05 09:45:48 +0300

    Добавлен класс модели пользователя User

---
+++ internet-shop_ct/Models/User.cs
@@ -0,0 +1,31 @@
+﻿namespace internet_shop_ct.Models
+{
+    public class User
+    {
+        public int User_code { get; private set; } //Код пользователя
+
+        public int Phone_number { get; private set; } //Номер телефона
+
+        public string Name { get; private set; } //Имя
+
+        public string Password { get; private set; } //Пароль
+
+        public DateOnly Registration_date { get; private set; } //Дата регистрации
+
+        public User(int user_code, int phone_number, string name, string password, DateOnly registration_date)
+        {
+            User_code = user_code;
+            Phone_number = phone_number;
+            Password = password;
+            Registration_date = registration_date;
+        }
+
+        public User(int phone_number, string name, string password)
+        {
+            Phone_number = phone_number;
+            Name = name;
+            Password = password;
+            Registration_date = DateOnly.FromDateTime(DateTime.Today);
+        }
+    }
+}
