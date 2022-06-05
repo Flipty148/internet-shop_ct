3f5a53133fea11ddace70622ac7b6e3604c098f3
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-05 14:08:56 +0300

    Добавлен класс модели категории Category

---
+++ internet-shop_ct/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace internet_shop_ct.Models
+{
+    public class Category
+    {
+        public int Id_Categories { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Category(int id_categories, string name)
+        {
+            Id_Categories = id_categories;
+            Name = name;
+        }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
