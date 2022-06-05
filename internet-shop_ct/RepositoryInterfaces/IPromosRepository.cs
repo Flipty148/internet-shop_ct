d13666ff2ce3a4dfb65fbd967add28e8ea98ffcf
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-05 12:17:25 +0300

    Добавлен интерфейс репозитория акций

---
+++ internet-shop_ct/RepositoryInterfaces/IPromosRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace internet_shop_ct.RepositoryInterfaces
+{
+    public interface IPromosRepository<T>
+    {
+        T GetById(T existingPromo);
+
+        internet_shop_ct.Models.Product[] GetProductsFromPromo(T existingPromo);
+
+        T[] GetCurrentPromos();
+    }
+}
