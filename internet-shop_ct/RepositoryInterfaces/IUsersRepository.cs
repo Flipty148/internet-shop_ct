1bbf8b7d54a388626e5a19763015f721260c0726
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-05 09:57:37 +0300

    Добавлен интерфейс репозитория пользователей

---
+++ internet-shop_ct/RepositoryInterfaces/IUsersRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace internet_shop_ct.RepositoryInterfaces
+{
+    public interface IUsersRepository<T>
+    {
+        T GetByUserCode(int user_code); //Получить пользователя по его коду
+
+        T GetByLogin(int phone_number, string password); //Получить пользователя по его данным (номер телефона и пароль)
+
+        T Add(T newUser); //Добавить пользователя
+
+        T Update(T existingUser); //Обновить данные пользователя
+
+        void Delete(T existingUser); //Удалить пользователя
+    }
+        
+}
