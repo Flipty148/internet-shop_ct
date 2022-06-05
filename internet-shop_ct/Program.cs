ffefcf66d2922b3bd852fe2ec3c19318e248939e
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-05 09:18:11 +0300

    Создание проекта

---
+++ internet-shop_ct/Program.cs
@@ -0,0 +1,15 @@
+using internet_shop_ct.Models;
+using internet_shop_ct.Repositories;
+
+namespace internet_shop_ct
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+
+       
+    }
+}
