996901ecc50f79c8db851f5955fce48afd927c8f
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-05 14:21:27 +0300

    Добавлен интерфейс репозитория категорий

---
+++ internet-shop_ct/RepositoryInterfaces/ICategoriesRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace internet_shop_ct.RepositoryInterfaces
+{
+    public interface ICategoriesRepository<T>
+    {
+        T[] GetAll(); //Получить все категории
+
+        internet_shop_ct.Models.Product[] GetProductsFromCategory(T existingCategory); //Получить все продукты из данной категории
+
+        T GetById(int id_category); //Получить категорию по её id
+    }
+}
