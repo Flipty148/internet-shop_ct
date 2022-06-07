c8ba278255b68fb631b0a11de32ac9fab13399dc
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-07 11:22:20 +0300

    Добавлен интерфейс репозитория заказов

---
+++ internet-shop_ct/RepositoryInterfaces/IOrdersRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace internet_shop_ct.RepositoryInterfaces
+{
+    public interface IOrdersRepository<T>
+    {
+        T Add(T newOrder, internet_shop_ct.Models.Product[] products, int userCode); //Добавление заказа
+
+        T Update(T existingOrder); //Обновление заказа (только пункта выдачи)
+
+        T Delete(T existingOrder); //Удаление заказа
+
+        internet_shop_ct.Models.Product[] GetProductsByOrder(T existingOrder); //Все товары в заказе
+
+        internet_shop_ct.Models.User GetUserByOrder(T existingOrder); //Пользователь по заказу
+
+        T[] GetOrdersByUser(int user_code); //Получить все заказы пользователя
+
+        T GetByOrderCode(int order_code); //Получить по коду заказа
+    }
+}
