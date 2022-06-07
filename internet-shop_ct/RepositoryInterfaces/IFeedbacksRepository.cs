6ee30106890fe063f3320ad9156fa4252a9e17a2
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-07 16:12:00 +0300

    Добавлен интерфейс репозитория отзывов

---
+++ internet-shop_ct/RepositoryInterfaces/IFeedbacksRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace internet_shop_ct.RepositoryInterfaces
+{
+    public interface IFeedbacksRepository<T>
+    {
+        T GetById(int id); //Получить по Id
+
+        T[] GetByUser(int user_code); //Получить все отзывы пользователя
+
+        T[] GetByProduct(int product_code); //Получить все отзывы для товара
+
+        float AvgRating(int product_code); //Получить среднюю оценку для продукта
+    }
+}
