e52213ecf49b6e8c012e7e40e91707b91877efba
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-07 09:30:20 +0300

    Добавлен интерфейс репозитория характеристик

---
+++ internet-shop_ct/RepositoryInterfaces/ICharacteristicsRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace internet_shop_ct.RepositoryInterfaces
+{
+    public interface ICharacteristicsRepository<T>
+    {
+        string[] GetValuesById(int id); //Получить все значения характеристики по её ID
+
+        T[] GetAllByProduct(internet_shop_ct.Models.Product existingProduct); //Получить все характеристики товара
+    }
+}
