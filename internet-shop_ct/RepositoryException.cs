6969aaac8397f5f66aae8eb37f4afba46656ca87
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-20 11:26:59 +0300

    Добавлен класс исключений в репозиториях RepositoryException

---
+++ internet-shop_ct/RepositoryException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace internet_shop_ct
+{
+    public class RepositoryException : Exception
+    {
+        public RepositoryException(int code, string message)
+        {
+            ErrorCode = code;
+            Message = message;
+        }
+
+        public RepositoryException(string message)
+        {
+            Message = message;
+        }
+
+        public int ErrorCode { get; private set; }
+        public string? Message { get; private set; }
+    }
+}
